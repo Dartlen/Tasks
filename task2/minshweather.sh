32923702a39745c95a7d075432d3809ced6c3c57
dartlen <dartlen@tut.by>
2017-05-14 22:40:49 +0300

    other cities added

--- task2/minshweather.sh
+++ task2/minshweather.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+$1
 
-for time in `cut -f 2 -d "="  config.ini `
+if [ -z "$*" ]; 
+	then $1 set "minsk"; echo "$1"
+	else echo "$1"
+fi
+
+for  time in `cut -f 2 -d "="  config.ini `
 do
    vtime=$time
 done
 
-echo "minsk"
 while [ : ]
 do
-	var=$(wget https://pogoda.tut.by/city/minsk -q -O - | grep -Po -m1 '[+-][1-9][0-9]')
+	var=$(wget https://pogoda.tut.by/city/$1 -q -O - | grep -Po -m1 '[+-][1-9][0-9]')
 	echo "$var"
 	sleep $vtime
 done
